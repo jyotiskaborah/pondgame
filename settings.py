4bac3de907ab2b980afd8fde11aa6e2742b25a19
Jyotiska Borah <jyotiskaborah@gmail.com>
2024-09-02 02:15:54 +0530

    all from pycharm

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
         """Initialise all settings"""
         self.screen_width = 1000
         self.screen_height = 700
-        self.bg_color = (29, 52, 56)
+        self.bg_color = (10, 31, 77)
 
 
         self.speed = 1.5
